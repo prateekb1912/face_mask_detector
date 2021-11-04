ccdcbedc7743ea0516b48022e6d9f8565dcd72d5
prateek-ml <prateekb1912@gmail.com>
2021-11-04 16:04:24 +0530

    Add preprocessing and detection functions

--- detect_mask_image.py
+++ detect_mask_image.py
@@ -25,18 +25,19 @@ args = vars(ap.parse_args())
 
 # Loading our serialized face detector model from disk
 print("[INFO] Loading face detector model...")
-protoTxtPath = os.path.sep.join([args["face"], "deploy.prototxt"])
-weightsPath = os.path.sep.join([args["face"],
-    "res10_300x300_ssd_iter_140000.caffemodel"])
-
-net = cv2.dnn.readNet(protoTxtPath, weightsPath)
 
 # Load the face mask detector model from disk
 print("[INFO] Loading the face mask detector model...")
 
 class MaskDetection:
     def __init__(self):
-        model = load_model(args["model"])
+        self.protoTxtPath = os.path.sep.join([args["face"], "deploy.prototxt"])
+        self.weightsPath = os.path.sep.join([args["face"], "res10_300x300_ssd_iter_140000.caffemodel"])
+
+        self.net = cv2.dnn.readNet(self.protoTxtPath, self.weightsPath)
+        self.model = load_model(args["model"])
+
+        self.detections = None
     
     def preprocess_image(self, image):
         orig = image.copy()
@@ -45,63 +46,65 @@ class MaskDetection:
         # Construct a blob from the image
         blob = cv2.dnn.blobFromImage(image, 1.0, (300, 300), (104.0, 177.0, 123.0))
 
+        # Pass the blob through the network and obtain the face detections
+        print("[INFO] Computing face detections...")
+        
+        self.net.setInput(blob)
+        self.detections = self.net.forward()
 
+    def process_detections(self, image):
+        # Loop over the detections
+        for i in range(0, self.detections.shape[2]):
+            # Extract the confidence (i.e. probability) associated with
+            # the detection
+            confidence = self.detections[0, 0, i, 2]
+
+            # Filter out weak detections by ensuring confidence is 
+            # greater than the minimum confidence
+            
+            if confidence > 0.5:
+                # Compute the (x, y)-coordinates of the bounding box for
+                # the object
+                box = self.detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                (startX, startY, endX, endY) = box.astype("int")
+
+                # Ensure the bounding boxes fall within the dimensions of
+                # the frame
+                (startX, startY) = (max(0, startX), max(0, startY))
+                (endX, endY) = (min(w-1, endX), min(h-1, endY))
+
+                # Extract the face ROI, convert it from BGR to RGB channel
+                # ordering, resize it to 224x224, and preprocess it
+                face = image[startY:endY, startX:endX]
+                face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+                face = cv2.resize(face, (224, 224))
+                face = img_to_array(face)
+                face = preprocess_input(face)
+                face = np.expand_dims(face, axis = 0)
+
+                # Pass the face through the model to determine if the face
+                # has a mask or not
+                (mask, withoutMask) = self.model.predict(face)[0]
+
+                # Determine the class label and color we'll use to draw 
+                # the bounding box and text
+                label = "Mask" if mask > withoutMask else "No Mask"
+                color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
+
+                # Include the probability in the label
+                label = "{} : {:.2f}".format(label, max(mask, withoutMask)*100)
+
+                # Display the label and bounding box rectangle on the ouput frame
+                cv2.putText(image, label, (startX, startY-10), cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+                cv2.rectangle(image, (startX, startY), (endX, endY), color, 2)
 
 # Load the input image from disk, clone it, and grab the image
 # spatial dimensions
 image = cv2.imread(args["image"])
 
-# Pass the blob through the network and obtain the face detections
-print("[INFO] Computing face detections...")
-net.setInput(blob)
-detections = net.forward()
 
 
-# Loop over the detections
-for i in range(0, detections.shape[2]):
-    # Extract the confidence (i.e. probability) associated with
-    # the detection
-    confidence = detections[0, 0, i, 2]
 
-    # Filter out weak detections by ensuring confidence is 
-    # greater than the minimum confidence
-    
-    if confidence > 0.5:
-        # Compute the (x, y)-coordinates of the bounding box for
-        # the object
-        box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-        (startX, startY, endX, endY) = box.astype("int")
-
-        # Ensure the bounding boxes fall within the dimensions of
-        # the frame
-        (startX, startY) = (max(0, startX), max(0, startY))
-        (endX, endY) = (min(w-1, endX), min(h-1, endY))
-
-        # Extract the face ROI, convert it from BGR to RGB channel
-        # ordering, resize it to 224x224, and preprocess it
-        face = image[startY:endY, startX:endX]
-        face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
-        face = cv2.resize(face, (224, 224))
-        face = img_to_array(face)
-        face = preprocess_input(face)
-        face = np.expand_dims(face, axis = 0)
-
-        # Pass the face through the model to determine if the face
-        # has a mask or not
-        (mask, withoutMask) = model.predict(face)[0]
-
-        # Determine the class label and color we'll use to draw 
-        # the bounding box and text
-        label = "Mask" if mask > withoutMask else "No Mask"
-        color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
-
-        # Include the probability in the label
-        label = "{} : {:.2f}".format(label, max(mask, withoutMask)*100)
-
-        # Display the label and bounding box rectangle on the ouput frame
-        cv2.putText(image, label, (startX, startY-10),
-            cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
-        cv2.rectangle(image, (startX, startY), (endX, endY), color, 2)
 
 # Show the output image
 cv2.imshow("Ouput", image)
