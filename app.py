639db16b4819e68fd59880e2b5845c954f55efbc
prateek-ml <prateekb1912@gmail.com>
2021-11-06 20:41:21 +0530

    Add function to predict the uploaded image

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import os
+import cv2
 from flask import Flask
 from flask import render_template, request
+from detector import MaskDetection
+
 
 app = Flask(__name__)
 UPLOAD_FOLDER = os.path.join(os.getcwd(), 'static')
@@ -12,8 +15,24 @@ def upload_predict():
         if image_file:
             img_loc = os.path.join(UPLOAD_FOLDER, image_file.filename)
             image_file.save(img_loc)
-            return render_template('index.html', prediction = 1)        
+            
+            pred = predict_image(image_file)
+            return render_template('index.html', prediction = pred)
+
     return render_template('index.html', prediction = 0)
+
+
+def predict_image(img_file):
+    image = cv2.imread(img_file)
+
+    detector = MaskDetection()
+    detector.preprocess_image(image)
+
+    label, prob, bbox = detector.process_detections(image)
+
+    return label
+
+
 
 if __name__ == '__main__':
     app.run(debug = True)
