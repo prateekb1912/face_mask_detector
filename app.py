00df60bcf5eff170e53a5eb3df1b5d3d2effdded
prateek-ml <prateekb1912@gmail.com>
2021-11-06 20:31:36 +0530

    Add function to upload image and save it

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from logging import debug
+import os
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 
 app = Flask(__name__)
+UPLOAD_FOLDER = os.path.join(os.getcwd(), 'static')
 
 @app.route("/", methods = ['GET', 'POST'])
 def upload_predict():
-    return render_template('index.html')
+    if request.method == 'POST':
+        image_file = request.files['image']
+        if image_file:
+            img_loc = os.path.join(UPLOAD_FOLDER, image_file.filename)
+            image_file.save(img_loc)
+            return render_template('index.html', prediction = 1)        
+    return render_template('index.html', prediction = 0)
 
 if __name__ == '__main__':
     app.run(debug = True)
