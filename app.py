a09f3a17755de37179a7bc9572927492b74419cd
prateek-ml <prateekb1912@gmail.com>
2021-11-06 20:19:12 +0530

    Create the flask app file

---
+++ app.py
@@ -0,0 +1,12 @@
+from logging import debug
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/", methods = ['GET', 'POST'])
+def upload_predict():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug = True)
